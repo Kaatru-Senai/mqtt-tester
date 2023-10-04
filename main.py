5aab5f3ac9efc980c417bb9f311727e93c14b149
suvindran3 <suvindran2001@gmail.com>
2023-10-04 16:50:47 +0530

    use bus waypoints

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import time
+import kml2geojson
+import pandas
 from streamer import Streamer
 
 
 def start_loop():
-    streamer.push('dev/cam1', {
-        'Id': 'xxx',
-        'Status': 'xxx',
-        'Timestamp': 'xxx',
-        'Price': 'xxx',
-        'Price/ml ': 'xxx',
-        'Manufacturing Date': 'xxx',
-        'Expiry Date': 'xxx',
-        'Batch No.': 'xxx',
-        'Other Code': 'xxx',
-        'Batch Time': 'xxx'
-    })
-    time.sleep(1)
-    start_loop()
+    i = 0
+    global randomizer
+    while i < len(bus_waypoints):
+        streamer.push('prod/chn/LM29/sen', {
+            'dID': 'LM29',
+            'sog': 4,
+            'lat': bus_waypoints[i][1],
+            'long': bus_waypoints[i][0]
+        })
+        if randomizer:
+            i -= 1
+            randomizer = False
+            print('INFO: Going backwards')
+        else:
+            i += 1
+            if i % 30 == 0:
+                randomizer = True
+            print('INFO: Going forwards')
+        time.sleep(5)
 
 
+randomizer = False
+bus_waypoints = [[row['value.lon'], row['value.lat']] for i, row in pandas.read_csv('LM30A.csv').iterrows()]
+# route = kml2geojson.convert('route.kml')[0]['features'][0]['geometry']['coordinates']
 streamer = Streamer(host='broker', port=1883)
 streamer.start()
 start_loop()
